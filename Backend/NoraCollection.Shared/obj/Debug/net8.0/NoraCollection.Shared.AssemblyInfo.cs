e2b060bba688cbeaeb404e89db954479814904c2
haticebasal1 <152809295+haticebasal1@users.noreply.github.com>
2025-12-16 13:48:32 +0300

    .

--- Backend/NoraCollection.Shared/obj/Debug/net8.0/NoraCollection.Shared.AssemblyInfo.cs
+++ Backend/NoraCollection.Shared/obj/Debug/net8.0/NoraCollection.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoraCollection.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5eaf2e62763b6f94489e1ec936e4896666d4189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e153f6266901e0b57d7c63375e7887260550627")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoraCollection.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoraCollection.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
