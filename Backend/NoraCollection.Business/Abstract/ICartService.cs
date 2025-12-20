bdefe56a6b1e129c67c7c30b0d5ed0d8a4f88459
haticebasal1 <152809295+haticebasal1@users.noreply.github.com>
2025-12-20 19:35:19 +0300

    .

---
+++ Backend/NoraCollection.Business/Abstract/ICartService.cs
@@ -0,0 +1,17 @@
+using System;
+using NoraCollection.Shared.Dtos.CartDtos;
+using NoraCollection.Shared.Dtos.ResponseDtos;
+
+namespace NoraCollection.Business.Abstract;
+
+public interface ICartService
+{
+ Task<ResponseDto<CartDto>> CreateCartAsync(CartCreateDto cartCreateDto);
+ Task<ResponseDto<CartDto>> GetCartAsync(string userId);
+ Task<ResponseDto<NoContentDto>> ClearCartAsync(string userId);
+ Task<ResponseDto<CartItemDto>> AddToCartAsync(AddToCartDto addToCartDto);
+ Task<ResponseDto<NoContentDto>> RemoveFromCartAsync(int cartItemId);
+ Task<ResponseDto<NoContentDto>> ChangeQuantityAsync(ChangeQuantityDto changeQuantityDto);
+ Task<ResponseDto<int>> GetCartItemCountAsync(string userId);
+ Task<ResponseDto<decimal>> GetCartTotalAsync(string userId);
+}
---
+++ Backend/NoraCollection.Business/Abstract/IImageService.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using NoraCollection.Shared.Dtos.ResponseDtos;
+
+namespace NoraCollection.Business.Abstract;
+
+public interface IImageService
+{
+    Task<ResponseDto<string>> UploadAsync(IFormFile image, string folderName);
+    Task<ResponseDto<List<string>>> UploadMultipleAsync(List<IFormFile> image, string folderName);
+    Task<ResponseDto<string>> ResizeAndUploadAsync(IFormFile image, string folderName);
+    ResponseDto<NoContentDto> DeleteImage(string imageUrl);
+    ResponseDto<bool> ImageExists(string imageUrl);
+}
