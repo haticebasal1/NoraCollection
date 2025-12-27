e6e4b84ef22ccde286f021c8f5d90d18605d1f2d
haticebasal1 <152809295+haticebasal1@users.noreply.github.com>
2025-12-27 22:26:58 +0300

    .

--- Backend/NoraCollection.Business/obj/Debug/net8.0/NoraCollection.Business.AssemblyInfo.cs
+++ Backend/NoraCollection.Business/obj/Debug/net8.0/NoraCollection.Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoraCollection.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06695e024345c0424ba7d61217f43207e5f26c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb31450dcbce93450d5a5e599a09041aca15f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoraCollection.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoraCollection.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/NoraCollection.Data/obj/Debug/net8.0/NoraCollection.Data.AssemblyInfo.cs
+++ Backend/NoraCollection.Data/obj/Debug/net8.0/NoraCollection.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoraCollection.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06695e024345c0424ba7d61217f43207e5f26c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb31450dcbce93450d5a5e599a09041aca15f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoraCollection.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoraCollection.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
